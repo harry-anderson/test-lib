2ba2957d2aef0d1022b0f8d77a1ae63cbab39ae6
Harry Anderson <harry@nuggets.life>
2024-03-05 16:02:41 +1100

    init

---
+++ ios/TestLib.swift
@@ -0,0 +1,8 @@
+@objc(TestLib)
+class TestLib: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
